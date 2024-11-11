edd14e8241853f4762be3b51b89455fa017e3e6f
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-11 19:37:52 +0100

    Showing data to the screen

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import Overview from "./screens/overview";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
-    <NavigationContainer>
-      {
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      }
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        {
+          <View style={styles.container}>
+            <Overview />
+          </View>
+        }
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
--- screens/overview.js
+++ screens/overview.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import movieData from "../data/data";
 
 export default function overview() {
   return (
-    <View>
-      <Text>overview</Text>
-    </View>
+    <FlatList
+      data={movieData}
+      renderItem={({ item }) => (
+        <View style={styles.container}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.releaseDate}>{item.director}</Text>
+        </View>
+      )}
+    ></FlatList>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e0e0e0",
+    backgroundColor: "#f9f9f9",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#333",
+  },
+  releaseDate: {
+    fontSize: 14,
+    color: "#666",
+  },
+});
