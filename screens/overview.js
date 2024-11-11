ce1d465e7c8857c53234c88691b6a392364836f7
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-11 19:20:54 +0100

    Installing dependencies

---
+++ screens/overview.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function overview() {
+  return (
+    <View>
+      <Text>overview</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
