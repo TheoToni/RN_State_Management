e01798e00efee4217f2da542c99844a56e6a5fe3
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-11-13 18:09:41 +0100

    Context progress1

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { createContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Overview from "./screens/Overview";
+import { AppProvider } from "./context/AppContext";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        {
+    <AppProvider>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
           <View style={styles.container}>
             <Overview />
           </View>
-        }
-      </NavigationContainer>
-    </SafeAreaView>
+        </NavigationContainer>
+      </SafeAreaView>
+    </AppProvider>
   );
 }
 
